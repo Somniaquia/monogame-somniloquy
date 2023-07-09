d2a2e6770916ea73e9687c1a45e60d9a2ec786fa
Somnia Quia <Somniaquia@gmail.com>
2023-07-09 21:49:10 +0900

    Added GameScreen and ScreenManager Added SoundManager (FMod control)

--- Somniloquy/Screens/GameScreen.cs
+++ Somniloquy/Screens/GameScreen.cs
@@ -5,24 +5,29 @@ namespace Somniloquy {
     using Microsoft.Xna.Framework;
     using Microsoft.Xna.Framework.Graphics;
     using Microsoft.Xna.Framework.Input;
-
+    using Microsoft.Xna.Framework.Media;
     using MonoGame.Extended;
     using MonoGame.Extended.BitmapFonts;
-    using MonoGame.Extended.Screens;
 
     public class GameScreen : Screen {
-        public static Dictionary<string, World> LoadedWorlds { get; private set; }
+        public List<World> LoadedWorlds { get; set; } = new();
+        public Camera Camera { get; set; } = new Camera(8.0f);
 
+        float pitch = 1.0f;
+        string music = "moseni_fog";
 
-        public static void LoadWorld(string worldName) {
-            LoadedWorlds.Add(worldName, SerializationManager.Deserialize<World>(worldName));
+        public void LoadWorld(string worldName) {
+            LoadedWorlds.Add(SerializationManager.Deserialize<World>(worldName));
         }
 
-        public static void UnloadWorld(string worldName) {
-            LoadedWorlds.Remove(worldName);
+        public void UnloadWorld(World world) {
+            LoadedWorlds.Remove(world);
         }
+
 
         public GameScreen(Rectangle boundaries) : base(boundaries) {
+            SoundManager.StartLoop(music, 2f);
+            SoundManager.CenterFrequency = 150f;
 
         }
 
@@ -31,17 +36,39 @@ namespace Somniloquy {
         }
 
         public override void Update() {
+            if (InputManager.IsKeyPressed(Keys.Enter)) {
+                SoundManager.StopLoop(music);
+                ScreenManager.ToEditorScreen(this);
+            }
+
+            if (InputManager.IsKeyDown(Keys.Left)) {
+                if (pitch > 0.1f) pitch -= 0.001f;
+                SoundManager.SetPitch(music, pitch);
+            } else if (InputManager.IsKeyDown(Keys.Right)) {
+                if (pitch < 2f) pitch += 0.001f;
+                SoundManager.SetPitch(music, pitch);
+            } else if (InputManager.IsKeyDown(Keys.Up)) {
+                SoundManager.CenterFrequency *= 1.001f;
+            }
+            else if (InputManager.IsKeyDown(Keys.Down)) {
+                SoundManager.CenterFrequency /= 1.001f;
+            }
+
             foreach (var entry in LoadedWorlds) {
-                entry.Value.Update();
+                entry.Update();
             }
         }
 
         public override void Draw() {
-            GameManager.SpriteBatch.Begin();
+            GameManager.SpriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp, transformMatrix: Camera.Transform);
 
-            // foreach (var entry in LoadedWorlds) {
-            //     entry.Value.Draw(camera);
-            // }
+            foreach (var world in LoadedWorlds) {
+                foreach (var layer in world.Layers) {
+                    layer.Draw(Camera);
+                }
+            }
+
+            GameManager.SpriteBatch.End();
         }
     }
 }
